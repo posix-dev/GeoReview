fecc59e715065c366d7694dabc663af2f9b2bdbe
pashko00710 <pashko00710@yandex.ru>
2020-05-24 17:02:53 +0300

    delete useless code

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -5,7 +5,6 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 // Main const
-// see more: https://github.com/vedees/webpack-template/blob/master/README.md#main-const
 const PATHS = {
     src: path.join(__dirname, '../src'),
     dist: path.join(__dirname, '../dist'),
@@ -13,7 +12,6 @@ const PATHS = {
 }
 
 // Pages const for HtmlWebpackPlugin
-// see more: https://github.com/vedees/webpack-template/blob/master/README.md#html-dir-folder
 // const PAGES_DIR = PATHS.src
 const PAGES_DIR = `${PATHS.src}/pug/pages/`
 const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'))
@@ -119,8 +117,6 @@ module.exports = {
         // ]),
 
         // Automatic creation any html pages (Don't forget to RERUN dev server)
-        // see more: https://github.com/vedees/webpack-template/blob/master/README.md#create-another-html-files
-        // best way to create pages: https://github.com/vedees/webpack-template/blob/master/README.md#third-method-best
         ...PAGES.map(page => new HtmlWebpackPlugin({
             template: `${PAGES_DIR}/${page}`,
             filename: `./${page.replace(/\.pug/, '.html')}`
